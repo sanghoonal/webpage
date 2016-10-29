9cf30980d7b618a481af03d54d3a1afdc78a1227
sanghoonal <sanghoonal@gmail.com>
2016-10-28 18:54:07 -0700

    smaller image

--- myscript.js
+++ myscript.js
@@ -3,7 +3,23 @@ function selectDelete() {
 	selectBox.remove(selectBox.selectedIndex)
 }
 function showImage() {
-	var img = document.createElement("img");
- 	img.src = "emma.jpg";
-	document.getElementById("imageHolder").appendChild(img);
+	var emmaImg = document.createElement("img");
+ 	emmaImg.src = "emma.jpg";
+ 	emmaImg.id = "emmaImage"
+ 	emmaImg.width = 1000;
+	document.getElementById("imageHolder").appendChild(emmaImg);
 } 
+
+function smallerImage() {
+	var small = document.getElementById("emmaImage").style.width="50%";
+	var t = document.getElementById("emmaImage")
+	t.appendChild(small)
+	document.getElementById(t).appendChild(small)
+	
+
+}
+
+/*var img = document.getElementById("img")
+	var w = img.width
+	w = "50%"
+	document.getElementById("imageHolder").appendChild(w);*/
