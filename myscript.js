131e0623a091b71c9940818477edd2f70c85b327
sanghoonal <sanghoonal@gmail.com>
2016-10-30 21:54:43 -0700

    add modal

--- myscript.js
+++ myscript.js
@@ -18,6 +18,42 @@ function smallerImage() {
 	
 
 }
+// var modal = document.getElementById("modalBox");
+// var btn = document.getElementById("modalBtn");
+// btn.onclick = function() {
+// 	console.log('modal', modal);
+// 	modal.style.display = "block";
+// 	}
+
+function openModal() {
+	var modal = document.getElementById("modalBox");
+	modal.style.display = "block";
+}
+
+/*function selectDelete() {
+	var selectBox = document.getElementById("cars");
+	selectBox.remove(selectBox.selectedIndex)
+}
+function showImage() {
+	var emmaImg = document.createElement("img");
+ 	emmaImg.src = "emma.jpg";
+ 	emmaImg.id = "emmaImage"
+ 	emmaImg.width = 1000;
+	document.getElementById("imageHolder").appendChild(emmaImg);
+} 
+
+function smallerImage() {
+	var small = document.getElementById("emmaImage").style.width="50%";
+	var t = document.getElementById("emmaImage")
+	t.appendChild(small)
+	document.getElementById(t).appendChild(small)
+	
+
+}
+
+function zoomOut() {
+
+}*/
 
 /*var img = document.getElementById("img")
 	var w = img.width
